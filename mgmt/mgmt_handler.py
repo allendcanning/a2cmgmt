e407cfa0c1253a502105ecdf009e050e9087a60b
Allen Canning <allen_canning@putnam.com>
2019-07-01 13:24:59 -0400

    Pulled head out of ass

--- mgmt/mgmt_handler.py
+++ mgmt/mgmt_handler.py
@@ -140,8 +140,8 @@ def print_email_templates(config,name):
   client = boto3.client('ses')
   tmpls = client.list_templates()
 
-  log_error("Got name: ",str(tmpls))
-  log_error("Got SES templates: ",str(tmpls))
+  log_error("Got name: "+str(name))
+  log_error("Got SES templates: "+str(tmpls))
 
   if name:
     # Add AJAX to get template info when the template name is changed
