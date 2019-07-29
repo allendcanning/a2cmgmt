d69678308c8387f6456dbeff9b4a68412e686e25
Allen Canning <allen_canning@putnam.com>
2019-07-29 10:21:35 -0400

    Fixed assignment of athlete info to email template data

--- mgmt/mgmt_handler.py
+++ mgmt/mgmt_handler.py
@@ -270,7 +270,8 @@ def send_email_template(config,record):
     coach = get_coach(config,to)
 
     for athlete in profiles:
-      template_data = athletes[athlete]
+      for item in athletes[athlete]:
+        template_data[item] = athletes[athlete][item]
       template_data['coachname'] = coach['first']+' '+coach['last']
       template_data['school'] = coach['school']
 
